ed8a95ad65ac7ad49fc41cf51f3c3cc1a6d6bc76
orient <orientye@qq.com>
2022-02-16 12:27:08 +0800

    schema-diff

--- dump_schema.sh
+++ dump_schema.sh
@@ -1,2 +1,2 @@
-mysqldump --no-data --events --triggers --routines --default-character-set=utf8 -B from_schema > test_from_schema.sql
-mysqldump --no-data --events --triggers --routines --default-character-set=utf8 -B to_schema > test_to_schema.sql
+mysqldump -u my_user -p --no-data --events --triggers --routines --default-character-set=utf8 -B from_schema > test_from_schema.sql
+mysqldump -u my_user -p --no-data --events --triggers --routines --default-character-set=utf8 -B to_schema > test_to_schema.sql
